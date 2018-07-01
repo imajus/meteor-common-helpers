92105e2e5f032fec3c096ce37e16b6e4838a6dcd
Denis Perov <denis.perov@gmail.com>
2018-07-01 23:02:27 +0300

    Increment package version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'imajus:common-helpers',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
-  summary: 'Logical operations, logging, object manipulation Spacebars helpers.',
+  summary: 'General helpers for Meteor Blaze including comparison, logic and numeric operators, logging, objects manipulation.',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/imajus/meteor-common-helpers.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: null//'README.md'
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
