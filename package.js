2c0793f54885018593ece06904491342dd099784
Denis Perov <denis.perov@gmail.com>
2024-08-17 21:34:23 +0500

    Support Meteor 3

--- package.js
+++ package.js
@@ -1,25 +1,20 @@
 Package.describe({
   name: 'imajus:common-helpers',
-  version: '0.0.3',
-  // Brief, one-line summary of the package.
-  summary: 'Meteor Blaze helpers for comparison, logic and numeric operators, logging.',
-  // URL to the Git repository containing the source code for this package.
+  version: '0.1.0',
+  summary: 'Blaze helpers for comparison, logic and numeric operators, logging',
   git: 'https://github.com/imajus/meteor-common-helpers.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2');
-  api.use('ecmascript@0.6.3');
-  api.use(['templating@1.3.0', 'underscore@1.0.10', 'imajus:helpers-core@0.0.1'], 'client');
+  api.versionsFrom(['2.0', '3.0']);
+  api.use('ecmascript');
+  api.use(['templating@1.3.4||1.4.4', 'underscore', 'imajus:helpers-core@0.1.0'], 'client');
   api.mainModule('common-helpers.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript@0.6.3');
-  api.use('tinytest@1.0.12');
-  api.use(['blaze@2.3.0', 'templating@1.3.0', 'imajus:common-helpers@0.0.1'], 'client');
+  api.use(['ecmascript', 'tinytest']);
+  api.use(['blaze', 'templating', 'imajus:common-helpers'], 'client');
   api.mainModule('common-helpers-tests.js', 'client');
 });
